a8832906129613199fd7717a541dfc601f43a444
Unknown <kayden18@ymail.com>
2017-01-30 10:56:34 -0700

    edit syntax file

---
+++ syntax.cs
@@ -0,0 +1,32 @@
+//Class identifiers are sentence casing
+//for more than 1 word titles UseCammelCasting
+
+public class SyntaxClass {
+    void ShootLaser() {
+        print("Hello World"); // each single line ends with ;
+        if (2 + 2 == 4) {
+            print("It's True");
+        }
+    }
+    void ForcePush () {
+        if (Yoda == real)
+        {
+            print("This is StarWars");
+        }
+        }
+    void zombieGun()
+    {
+        if(ammo == 100)
+        {
+            print("Clip is full");
+        }
+    }
+    }
+    // = is an assigning operator 
+    // == is a comparison operator (compares two peices of data)
+    
+}
+
+// every block of code needs {}
+// every funtion and loop need ()
+
