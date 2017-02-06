08d880626dc8d4b8aea1e359eb8d2185ea397d51
Unknown <kayden18@ymail.com>
2017-02-06 10:04:32 -0700

    mathoperators

---
+++ scripting2017/scripting2017/Assets/mathoperator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MathOperators : MonoBehaviour{
+
+    public int addition = 12 + 4;
+    public int subtraction = 12 - 4;
+    public float division = 5.0f / 2.0f;
+    public int multiplication = 10 * 2;
+    public int remainder = 7 % 3;
+
+	// Use this for initialization
+	void Start () {
+        addition += 9;//+= means add two
+        subtraction -= 10; //-= subtract 2
+        addition += 3;
+        addition = 1; // = assigns
+	}
+
+}
