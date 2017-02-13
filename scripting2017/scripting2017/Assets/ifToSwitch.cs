956573a2d8aba6f6329c4d54fd2ddf418b26f881
Unknown <kayden18@ymail.com>
2017-02-13 10:53:34 -0700

    switch and loop

---
+++ scripting2017/scripting2017/Assets/ifToSwitch.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ifToSwitch : MonoBehaviour {
+
+	// Use this for initialization;
+	void Start () {
+	
+	    //check for true with one object;
+        if(2 + 2 == 4)
+        {
+            print(true);
+        }
+	    if(2 + 2 != 5)
+        {
+            print(true);
+        }
+
+        int nums = 2 + 2;
+        //check for true and false in a single statement;
+        if (nums == 5)
+        {
+            print(true);
+        }
+        else
+        {
+            print(false);
+        }
+        //check for 3 or more values;
+        switch (nums)
+        {
+            case 6:
+                print("Well done you have " + nums);
+                break;
+            case 7:
+                print("Too bad you have " + nums);
+                break;
+            case 8:
+                print("Stop drilling, you have " + nums);
+                break;
+            default:
+                print(nums);
+                break;
+        }
+	}
+}
---
+++ scripting2017/scripting2017/Assets/loops.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loops : MonoBehaviour {
+
+    public string[] animals;
+    public string animal = "Wolf";
+
+	void Start () {
+        foreach (string animalInAnimals in animals)
+        {
+            if (animal == animalInAnimals)
+            {
+                print("I have a match, it is a " + animalInAnimals);
+            }
+            else
+            {
+                print(animalInAnimals + " is not a match.");
+            }
+        }
+	}
+}
