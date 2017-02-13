6332448537c30a9c692f033812713df486d8e8e7
Unknown <kayden18@ymail.com>
2017-02-13 13:05:53 -0700

    remote fix

---
+++ scripting2017/scripting2017/Assets/switchWhile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchWhile : MonoBehaviour {
+
+    // switch statements run through the code once searching for specific variables that are string types unless used with an enum.
+    public enum powerUps {FLIGHT, STREGTH, SPEED};
+    public powerUps playerPowerUp;
+    void Start () {
+        switch (playerPowerUp){
+        case powerUps.FLIGHT:
+                print("player can fly");
+        break;
+        case powerUps.STREGTH:
+                print("player can lift heavy objects");
+        break;
+            case powerUps.SPEED:
+                print("play can move quicker");
+                break;
+        }
+		
+	}
+}
