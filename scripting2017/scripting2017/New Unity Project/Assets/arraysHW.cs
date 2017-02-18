1fdac4524b39d910ed873d3e7287c45eb789d2ac
Unknown <kayden18@ymail.com>
2017-02-17 19:21:33 -0700

    arrays

---
+++ scripting2017/scripting2017/New Unity Project/Assets/arraysHW.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arraysHW : MonoBehaviour {
+
+    //arrays hold a group of strings and ints that can be accessed by functions anywhere in the code;
+
+    int i = 0;
+    string[] names = new string[] { "Matt", "Joanne", "Robert", "Ian", "James" };
+
+    void Start () {
+        while (i < names.Length)
+        {
+            print(names[i]);
+            i++;
+        }
+        print("Greetings");
+
+    }
+}
