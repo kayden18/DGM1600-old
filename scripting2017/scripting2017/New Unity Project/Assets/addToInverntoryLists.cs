ca0130e51cc4792f9620d279251f65039afd7eab
Unknown <kayden18@ymail.com>
2017-02-22 11:00:51 -0700

    lists

---
+++ scripting2017/scripting2017/New Unity Project/Assets/addToInverntoryLists.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class addToInverntoryLists : MonoBehaviour
+{
+
+    public Lists myInventory;
+    public string myWeapon;
+
+    public void addToList()
+    {
+        myInventory.myWeaponLists.Add(myWeapon);
+    }
+}
---
+++ scripting2017/scripting2017/New Unity Project/Assets/inputaddtolist.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class inputaddtolist : MonoBehaviour
+{
+
+    public List<string> myWeaponLists;
+    public string[] myWeaponArray;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            myWeaponLists.Add("Lance");
+        }
+        if (Input.GetKey(KeyCode.B))
+        {
+            myWeaponLists.Add("Club");
+        }
+    }
+}
