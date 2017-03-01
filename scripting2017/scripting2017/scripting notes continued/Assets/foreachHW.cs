e106db2e366cd79a9f73837ccbf7f649c5c742e9
Unknown <kayden18@ymail.com>
2017-03-01 15:50:32 -0700

    foreachhw

---
+++ scripting2017/scripting2017/scripting notes continued/Assets/foreachHW.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class foreachHW : MonoBehaviour
+{
+    public List<GameObject> superPowerList;
+    public List<GameObject> strengthList;
+    public List<GameObject> speedList;
+    public List<GameObject> flightList;
+
+
+    public void CheckDate()
+    {
+        foreach (GameObject item in superPowerList)
+        {
+
+            switch (item.name)
+            {
+                case "stregth":
+                    strengthList.Add(item);
+                    break;
+                case "Sphere":
+                    speedList.Add(item);
+                    break;
+                case "Capsule":
+                    flightList.Add(item);
+                    break;
+            }
+        }
+    }
+}
