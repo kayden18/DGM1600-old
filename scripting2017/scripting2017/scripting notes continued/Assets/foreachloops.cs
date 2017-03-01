d7d6c73369e407309867673b221851855d1cb5d7
Unknown <kayden18@ymail.com>
2017-03-01 10:47:27 -0700

    foreachloops

---
+++ scripting2017/scripting2017/scripting notes continued/Assets/foreachloops.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class foreachloops : MonoBehaviour {
+
+    public List<GameObject> powerUpList;
+    public List<GameObject> cubeList;
+    public List<GameObject> sphereList;
+    public List<GameObject> capsuleList;
+
+	public void CheckDate() {
+        foreach (GameObject item in powerUpList)
+        {
+            switch (item.name)
+            {
+                case "Cube":
+                    cubeList.Add(item);
+                    break;
+                case "Sphere":
+                    sphereList.Add(item);
+                    break;
+                case "Capsule":
+                    capsuleList.Add(item);
+                    break;
+            }
+        }
+	}
+}
