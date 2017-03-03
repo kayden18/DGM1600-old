a54e4b81df50bef0a422b14d1a2b46874b4d54cd
Unknown <kayden18@ymail.com>
2017-03-03 10:39:18 -0700

    logical operators

---
+++ scripting2017/scripting2017/scripting notes continued/Assets/buttonClick.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonClick : MonoBehaviour {
+
+
+	public void OnButtonClick () {
+        print("click");
+	}
+	
+}
---
+++ scripting2017/scripting2017/scripting notes continued/Assets/logicalOperators.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class logicalOperators : MonoBehaviour {
+
+    public string string1;
+    public string string2;
+	
+	public void OnButtonClick () {
+        if (string1 == "thing 1" && string2 == "Thing 2")
+        {
+            print("Things are quite tame.");
+        }
+        if(string1 == "Thing 1" || string2 == "Thing 2")
+        {
+            print("It's fun to have fun if you know what to do");
+        }
+        if(string1 != "Thing 1" && string2 != "Thing 2")
+        {
+            print("Oh what a shame");
+        }
+	}
+	
+}
