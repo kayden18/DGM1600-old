c5de2e7914c4e40e1ac5f36d798a5c38911bdeed
Unknown <kayden18@ymail.com>
2017-03-08 10:53:04 -0700

    scope and access and lists

---
+++ scripting2017/scripting2017/scripting notes continued/Assets/ListScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ListScript : MonoBehaviour {
+
+    public List<string> myFruit;
+
+    void Start()
+    {
+        myFruit.Add("Red Fruit");
+    }
+	
+}
---
+++ scripting2017/scripting2017/scripting notes continued/Assets/publicvsprivate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class publicvsprivate : MonoBehaviour {
+
+    int score = 100;
+    private int health = 100;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+}
