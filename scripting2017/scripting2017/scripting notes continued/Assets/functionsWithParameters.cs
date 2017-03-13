fb5e2297d08eeb8f619746e6425ccf3f7d1d250b
Unknown <kayden18@ymail.com>
2017-03-13 10:54:28 -0600

    functions with parameteres

---
+++ scripting2017/scripting2017/scripting notes continued/Assets/functionsWithParameters.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class functionsWithParameters : MonoBehaviour {
+
+    public int health = 100;
+
+    void Start()
+    {
+        AddHealth(50);
+        print(health);
+        AddHealth(25);
+        print(health);
+        ReplaceHealth(25, 20);
+        print(health);
+    }
+
+    public void AddHealth(int _h)
+    {
+        health += _h;
+    }
+    public void ReplaceHealth(int _a, int _b)
+    {
+        health = _a + _b;
+    }
+
+    public void WelcomePlayer(string _name)
+    {
+        print("Welcome " + _name + ", it's good to have you.");
+    }
+
+    public string userName;
+    public InputField myInputField;
+
+    public void updateStringValue(string _s)
+    {
+        userName += _s;
+        print(userName);
+    }
+
+    public void updateUserName()
+    {
+        WelcomeUser(myInputField.text);
+    }
+    void WelcomeUser (string _name)
+    {
+        userName = _name;
+        print("Welcome " + _name + ", get ready to play");
+
+    }
+}
