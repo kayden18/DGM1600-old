4aa4d3bfe2567db3e57bc200fc8ac90bc5ba49dd
Unknown <kayden18@ymail.com>
2017-03-15 11:02:53 -0600

    returns

---
+++ scripting2017/scripting2017/scripting notes continued/Assets/functionWithReturn.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class functionWithReturn : MonoBehaviour
+{
+
+    public GameObject player1;
+    public GameObject player2;
+    public GameObject player3;
+    public GameObject firstPlayer;
+
+    public string[] deepThoughts;
+    public int i = 0;
+
+    GameObject ChooseFirstPlayer()
+    {
+        int ranNum = Random.Range(1, 3);
+
+        switch (ranNum)
+        {
+            case 1:
+                return player1;
+            case 2:
+                return player2;
+            default:
+                return player3;
+        }
+    }
+
+    private string Messages()
+    {
+        if (i >= deepThoughts.Length)
+        {
+            i = 0;
+        }
+        return(deepThoughts[i++]);
+
+    }
+    public void onButtonClick()
+    {
+        print(Messages());
+    }
+    void Start()
+    {
+        firstPlayer = ChooseFirstPlayer();
+    }
+}
---
+++ scripting2017/scripting2017/scripting notes continued/Assets/returnFunction.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class returnFunction : MonoBehaviour {
+
+    int score = 100;
+    private int health = 100;
+
+    void Start()
+    {
+        float[] scores = { 24f, 60f, 100f, 86f };
+        print(Average(scores));
+    }
+
+    float Sum(float num1, float num2)
+    {
+        float sum;
+        sum = num1 + num2;
+        return sum;
+    }
+
+    float Average(float[] scores)
+    {
+        float sum = 0;
+        float average;
+        foreach (float score in scores)
+        {
+            sum += score;
+        }
+
+        average = sum / scores.Length;
+
+        return average;
+    }
+}
+
