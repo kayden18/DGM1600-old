3bb074ee0882da4ab2c374a62f643c9944e85b38
Unknown <kayden18@ymail.com>
2017-03-31 11:10:42 -0600

    vendingmachine

---
+++ scripting2017/scripting2017/scripting notes continued/Assets/review/MoneyInput.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MoneyInput : MonoBehaviour {
+
+    public InputField money;
+    public float MoneyToFloat()
+    {
+        return float.Parse(money.text);
+    }
+}
---
+++ scripting2017/scripting2017/scripting notes continued/Assets/review/VendingButton.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class VendingButton : MonoBehaviour {
+
+    public Text buttonLable;
+
+    void Start()
+    {
+        buttonLable.text = productName + " $" + productPrice;
+        //transform.parent.name = productName + " $" + productPrice; <- customise buttons
+    }
+
+    public MoneyInput moneyInput;
+
+    public string productName = "Coke";
+    public float productPrice = 1.5f;
+
+	float vendProduct(float money)
+    {
+        float change = 0;
+        if(money >= productPrice)
+        {
+            print("Vending " + productName + ".");
+            change = money - productPrice;
+        }
+
+        return change;
+    }
+
+    public void buttonClick()
+    {
+        print("$" + vendProduct(moneyInput.MoneyToFloat()) + " is your change");
+    }
+}
